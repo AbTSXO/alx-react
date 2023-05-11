6e405a30dc3e95607162459268c1caa9bb4d6846
AbTSXO <ashenagfioa@gmail.com>
2023-05-11 12:32:26 +0000

    fdgxc

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,7 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const mO = fromJS(object);
+  const mappedObj = fromJS(object);
 
-  return mO.getIn(array, undefined);
+  return mappedObj.getIn(array, undefined);
+}
